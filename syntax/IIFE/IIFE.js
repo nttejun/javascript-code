f78f0cdcfca7a390859155eb12347782b18c262b
Wonjun Jeong <nttejun@gmail.com>
2017-12-19 19:31:00 +0900

    IIFE 즉시함수호출

--- syntax/IIFE/IIFE.js
+++ syntax/IIFE/IIFE.js
@@ -1,6 +1,6 @@
 
 // IIFE 함수를 블록처럼 사용
-// IIFE 정의하고 바로 호출하는 즉시함수호출
+// IIFE 정의하고 바로 호출하는 즉함수호출
 (function () {
     var tmp = "Test";
 }());
@@ -21,3 +21,17 @@ for(var i=0; i<5; i++){
     notIife.push(function(){return i});
 }
 console.log(notIife[1]());  // 5
+
+/*
+    즉시함수호출이 없다면 반복문 안에서 벌어지는 일
+    반복문 종료 후 함수가 실행된다
+    notIife = [
+        function() {return i;},
+        function() {return i;},
+        function() {return i;},
+        function() {return i;},
+        function() {return i;}
+   ];
+
+
+ */
