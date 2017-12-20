e4aa3fa8996df6d2f3285e327606399848cba5cf
Wonjun Jeong <nttejun@gmail.com>
2017-12-21 08:55:55 +0900

    const 할당된 값은 변경할 수 없다

--- ES6/const/const.js
+++ ES6/const/const.js
@@ -27,3 +27,15 @@ const CONST_VAL = "CONST_VAL";
 }());
 
 console.log(CONST_VAL); // CONST_VAL
+
+const arr = ['js', 'script', 'java'];
+
+/*
+    // const 할당된 메모리를 재할당 할 수 없다
+    // 컴파일 시점 에러
+    arr = ['js', 'script', 'java'];
+*/
+
+arr.push('java');
+
+console.log(arr);   // ['js', 'script', 'java', 'java']
