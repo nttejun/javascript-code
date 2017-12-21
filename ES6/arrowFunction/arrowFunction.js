7e0118c63ffb16769ed13b2fb052a809b1b282bf
Wonjun Jeong <nttejun@gmail.com>
2017-12-21 09:15:40 +0900

    arrow function define

---
+++ ES6/arrowFunction/arrowFunction.js
@@ -0,0 +1,18 @@
+// arrow function define
+var arrowFunction = () => {
+    console.log("arrow function");
+}
+
+var arrowFuntionParam = (a, b) => {
+    console.log(a, b);
+}
+
+// existing function define
+var nomalFunction = function nomalFunction() {
+    console.log("nomal function");
+}
+
+var nomalFunctionParam = function nomalFunctionParam (a, b) {
+    console.log(a, b);
+}
+    
