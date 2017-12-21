e452715daf9855574e86cb5c203c0038d0ab8a23
Wonjun Jeong <nttejun@gmail.com>
2017-12-21 09:28:58 +0900

    Arrow function define && How to use default parameter

--- ES6/arrowFunction/arrowFunction.js
+++ ES6/arrowFunction/arrowFunction.js
@@ -1,18 +1,32 @@
 // arrow function define
-var arrowFunction = () => {
+let arrowFunction = () => {
     console.log("arrow function");
 }
 
-var arrowFuntionParam = (a, b) => {
+let arrowFuntionParam = (a, b) => {
     console.log(a, b);
 }
 
 // existing function define
-var nomalFunction = function nomalFunction() {
+let nomalFunction = function nomalFunction() {
     console.log("nomal function");
 }
 
-var nomalFunctionParam = function nomalFunctionParam (a, b) {
+let nomalFunctionParam = function nomalFunctionParam (a, b) {
     console.log(a, b);
 }
     
+let defaultParameterB = (a = 10, b) => {
+    return a + b;
+}
+
+let defaultParameterA = (a, b = 10) => {
+    return a + b;
+}
+
+// default Parameter fail case
+defaultParameterB(100);  // 100
+
+// default Parameter success case
+defaultParameterA(100);  // 110
+
