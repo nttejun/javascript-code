c2e47f7cb36bf0557bcde6658fba750afb0a7864
Wonjun Jeong <nttejun@gmail.com>
2017-12-22 09:51:17 +0900

    Add for .. of ..  Case

--- ES6/forLoop/forLoop.js
+++ ES6/forLoop/forLoop.js
@@ -1,4 +1,5 @@
-// array
+// for .. of .. Case
+// Case1 Basic
 (function () {
 
     let arr = [1, 2, 3, 4, 5];
@@ -9,11 +10,48 @@
 
 }());
 
-// ...array max
+// Case2 Find sum value
+(function () {
+
+    let sum = 0;
+    let arr = [1, 2, 3, 4, 5];
+
+    for (let index of arr) {
+        sum += index;
+    }
+    console.log(sum);
+})
+
+// Case3 Find max value
 (function () {
 
     let arr = [1, 2, 3, 4, 5];
-
     console.log(Math.max(...arr));
+
+}());
+
+// Case4 Use arrow function
+(function () {
+
+    let arr = (arr) => {
+        let sum = 0;
+        for (let index of arr)
+            sum += index;
+        return sum;
+    }
+    console.log(arr([10, 20, 30, 40, 50]));
+
+}());
+
+// Case5 Use ... to make object
+(function () {
+
+    let arr = (...arr) => {
+        let sum = 0;
+        for (let index of arr)
+            sum += index;
+        return sum;
+    }
+    console.log(arr(1, 2, 3, 4, 5));
 
 }());
