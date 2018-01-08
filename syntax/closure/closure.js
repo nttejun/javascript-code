9d7795656f9831eed151040891ec654bc975c56f
Wonjun Jeong <nttejun@gmail.com>
2018-01-08 15:51:42 +0900

    private variable and IIFE

--- syntax/closure/closure.js
+++ syntax/closure/closure.js
@@ -20,7 +20,7 @@ function outter() {
     otherOutter();
 }
 
-
+// 외부함수 내부함수로 접근불가
 function otherOutter() {
     function otherInner(){
         var title = "other inner";
@@ -29,5 +29,68 @@ function otherOutter() {
     otherInner();
 }
 
-// error
+// error case
 // otherInner ()
+
+
+// 내부함수 외부함수로 접근가능
+function out () {
+    var outVar = "out";
+    function outInner (){
+        alert(outVar);
+        console.log(otherOut());
+    }
+    outInner();
+}
+
+function otherOut(){
+    return "otherOut";
+}
+
+// private variable case
+function factory_user(person) {
+    return {
+        get_person: function () {
+            return person;
+        },
+        set_person: function (setPerson) {
+            person = setPerson;
+        }
+    }
+}
+
+wonjun = factory_user("wonjun");
+jeeyoen = factory_user("jeeyeon");
+
+alert(wonjun.get_person());
+alert(jeeyoen.get_person());
+
+wonjun.set_person("wonjun2");
+alert(wonjun.get_person());
+
+
+// 일반함수호출실행
+var arr = [];
+for(var i=0; i<5; i++){
+    arr[i] = function(){
+        return i;
+    }
+}
+for(var index in arr){
+    console.log(arr[index]);
+    alert(arr[index]());
+}
+
+// 즉시함수호출실행
+var arr1 = [];
+for(var i=0; i<5; i++){
+    arr1[i] = function(value){
+        return function(){
+            return value;
+        }
+    }(i);
+}
+
+for(var index in arr1){
+    alert(arr1[index]);
+}
