6e1cafc4ca4ba653a8ef83cea004eaa917276702
Wonjun Jeong <nttejun@gmail.com>
2018-01-08 11:36:49 +0900

    inner function

--- syntax/closure/closure.js
+++ syntax/closure/closure.js
@@ -9,3 +9,25 @@ function create(value){
 var output = create(1);
 output(); // 1
 output(); // 2
+
+// 함수 내부함수 실행
+function outter() {
+    function inner(){
+        var title = "inner";
+        alert(title);
+    }
+    inner();
+    otherOutter();
+}
+
+
+function otherOutter() {
+    function otherInner(){
+        var title = "other inner";
+        alert(title);
+    }
+    otherInner();
+}
+
+// error
+// otherInner ()
