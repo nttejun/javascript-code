86fb3423b3ffbfbcc85867f16f3910bf9b533b7c
Wonjun Jeong <nttejun@gmail.com>
2018-01-17 22:58:01 +0900

    Type

---
+++ syntax/Type/type.js
@@ -0,0 +1,23 @@
+var formData = {width : '100'};
+var w = formData.width;
+var outer = w + 20;
+
+console.log(outer === 120);
+console.log(outer === '10020');
+
+var nw = Number(formData.width);
+
+console.log(10 + nw);
+console.log(nw + nw);
+
+var str = String('str');
+
+console.log(str);
+console.log(typeof str);
+
+var strNumber = String('12.345');
+console.log(strNumber + 12.345);
+console.log(typeof (strNumber + 12.345));
+
+var obj = { foo : 123 };
+console.log(Object(obj) === obj);
